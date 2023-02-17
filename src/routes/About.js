fa5c0563fe852929112274f33946663a567fca91
hojin535 <hojin535@naver.com>
2023-02-17 12:16:01 +0900

    class version

---
+++ src/routes/About.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function About(props) {
+  console.log(props);
+  return <span>About this page: I bult it because I love movies</span>;
+}
+export default About;
---
+++ src/routes/Detail.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+function Detail() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (location.state === null) {
+      navigate("/");
+    }
+  });
+
+  if (location.state) {
+    const { title } = location.state;
+    return <span>{title}</span>;
+  }
+  return null;
+}
+export default Detail;
